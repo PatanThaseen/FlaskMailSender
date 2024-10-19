341db4279de6f25879600245f49d831b238df736
PatanThaseen <patanthaseen2004@gmail.com>
2024-10-19 22:33:21 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request, redirect
+from flask_mail import Mail, Message
+
+app = Flask(__name__)
+
+# Flask-Mail configuration
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = 'examplemail@gmail.com' #Replace with your mail
+app.config['MAIL_PASSWORD'] = 'example_password'  # Replace with App Password
+
+mail = Mail(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/send-email', methods=['POST'])
+def send_email():
+    email = request.form['email']
+    msg = Message('Hello from Flask', sender='your-email@gmail.com', recipients=[email])
+    msg.body = 'This is a test email sent from a Flask app using Gmail.'
+    mail.send(msg)
+    return 'Email sent!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
